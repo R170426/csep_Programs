3116b4204456f2f323f271e9b1eb336ac7617f1f
R170426 <107173815+R170426@users.noreply.github.com>
2022-06-09 13:18:27 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -3,13 +3,13 @@ b=str(input("Enter the input str2:"))
 str2_dict={}
 str1_dict={}
 for i in a:
-    print(i)
-    r=l.count(i)
-    print(r)
+
+    r=a.count(i)
+ 
     if i not in str1_dict:
         str1_dict[i]=r
 for j in b:
-    s=m.count(j)
+    s=b.count(j)
     if j not in str2_dict:
         str2_dict[j]=s
 print(str1_dict)
@@ -18,3 +18,4 @@ if str1_dict.items()==str2_dict.items():
     print("It is an anagram")
 else:
     print("not anagram")
+    
